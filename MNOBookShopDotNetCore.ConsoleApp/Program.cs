7ac99d4aa9f60e7710ebae72cfc928d9f114bfb9
MratRikzil <142309328+MratRikzil@users.noreply.github.com>
2024-04-11 22:07:20 +0630

    Done.

--- MNOBookShopDotNetCore.ConsoleApp/Program.cs
+++ MNOBookShopDotNetCore.ConsoleApp/Program.cs
@@ -4,13 +4,15 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+
         AdoBookDotNetExample adoBookDotNetExample = new AdoBookDotNetExample();
         // adoBookDotNetExample.Update(7, "Patrick King", "Real People Like a Book", 400, "06 / 10 / 2020");
         //adoBookDotNetExample.Delete(7);
         //adoBookDotNetExample.Read();
         // adoBookDotNetExample.Create(7, "Patrick King", "Real People Like a Book", 400, "10/06/2020");
-       // adoBookDotNetExample.Search(7);
-        adoBookDotNetExample.Search(1);
+        // adoBookDotNetExample.Search(7);
+        int input = Convert.ToInt32(Console.ReadLine());
+        adoBookDotNetExample.Search(input);
         Console.ReadKey();
 
     }
